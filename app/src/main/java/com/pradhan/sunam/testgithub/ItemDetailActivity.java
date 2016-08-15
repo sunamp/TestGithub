f6d8f1e2baafcc69eaa725ce28cc0cef30ee0f6e
Sunam <sunamp@gmail.com>
2016-08-15 23:52:32 +1000

    tiny changes

--- app/src/main/java/com/pradhan/sunam/testgithub/ItemDetailActivity.java
+++ app/src/main/java/com/pradhan/sunam/testgithub/ItemDetailActivity.java
@@ -25,6 +25,8 @@ public class ItemDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
 
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
